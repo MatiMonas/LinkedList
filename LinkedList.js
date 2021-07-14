b0f6869c452419c79ccadd0f729af30fe9c973a7
matimonas <matutin97@gmail.com>
2021-07-14 14:48:57 -0300

    add new functions

--- LinkedList.js
+++ LinkedList.js
@@ -13,13 +13,31 @@ class Node {
      }
 }
 
+/*-----------------
+Methods you can use
+
+push()
+unshift()
+insertAt()
+size()
+remove()
+removeFromHead()
+removeFrom()
+removeData()
+search()
+orderMinToMax()
+reverseMaxToMin()
+print()
+
+------------------*/
+
 class LinkedList {
      constructor() {
           this.head = null;
           this._length = 0;
      }
 
-     add(data) {
+     push(data) {
           let current = this.head;
 
           if (!current) {
@@ -34,9 +52,49 @@ class LinkedList {
           }
      }
 
+     unshift(data) {
+          var newNode = new Node(data);
+          let current = this.head;
+          if (!current) {
+               this.head = newNode;
+          } else {
+               this.head = newNode;
+               newNode.next = current;
+          }
+          this._length++;
+     }
+
+     insertAt(data, pos) {
+          let current = this.head;
+          let newNode = new Node(data);
+
+          if (this._length < pos) return this.push(data);
+
+          if (pos < 0)
+               throw TypeError(`Position shouldn't be a negative number`);
+
+          if (pos === 0 || !pos) {
+               this.unshift(data);
+
+               if (pos === 1) {
+                    newNode.next = this.head;
+                    this.head = newNode;
+               }
+          } else {
+               while (pos > 1) {
+                    pos--;
+                    current = current.next;
+               }
+               newNode.next = current.next;
+               current.next = newNode;
+          }
+          this._length++;
+     }
+
      size() {
           return this._length;
      }
+
      remove() {
           let current = this.head;
           let previous = current;
@@ -47,13 +105,13 @@ class LinkedList {
 
           if (current.next === null) {
                this.head = null;
-               return current.value;
+               return current.data;
           }
 
           while (current) {
                if (!current.next) {
                     previous.next = null;
-                    return current.value;
+                    return current.data;
                }
                previous = current;
                current = current.next;
@@ -76,6 +134,33 @@ class LinkedList {
                count++;
           }
           current.next = current.next.next;
+     }
+
+     removeData(data) {
+          let current = this.head;
+          let previous = current;
+
+          if (!current) {
+               return null;
+          }
+
+          if (current.data === data) {
+               this.head = current.next;
+          }
+
+          while (current) {
+               if (current.data === data) {
+                    var dato = current.data;
+                    previous.next = current.next;
+                    current = null;
+
+                    return dato;
+               }
+               previous = current;
+               current = current.next;
+          }
+
+          return null;
      }
 
      search(data, cb) {
